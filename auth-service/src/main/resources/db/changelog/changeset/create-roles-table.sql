bcb4ed41ea3ea7ac8fbd93fce69f84409188073b
qufull <oleza539@gmail.com>
2025-10-02 01:54:17 +0300

    Api - Gateway  аутентификация через keycloak хранение токенов в Redis автоматическое обновление access_token делает logout с чисткой сессии и Keycloak

--- auth-service/src/main/resources/db/changelog/changeset/create-roles-table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE roles (
-                       id SERIAL PRIMARY KEY,
-                       name VARCHAR(50) UNIQUE NOT NULL
-);
--- auth-service/src/main/resources/db/changelog/changeset/create-user_roles-table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE user_roles (
-                            user_id UUID REFERENCES users(id),
-                            role_id INTEGER REFERENCES roles(id),
-                            PRIMARY KEY (user_id, role_id)
-);
